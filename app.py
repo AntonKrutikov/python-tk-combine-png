d182230bcfabcdff27736a0219b8cb8eb453eb63
antonkrutikov <kap@reso-med.com>
2021-11-30 21:33:49 +0300

    Fix rigth list behaviour on windows

--- app.py
+++ app.py
@@ -104,8 +104,10 @@ def update_image(e=None):
 
 update_image()
 
-frame = VerticalScrolledFrame(master=window, highlightthickness=1, borderwidth=1, relief="groove")
+frame = tk.Frame(master=window, highlightthickness=1, borderwidth=1, relief="groove")
 frame.grid(row=0,column=1, sticky='nwse')
+
+frame.columnconfigure(1, weight=1, minsize=150)
 
 def btn_left_handler(label, layer):
     indx = layer['files'].index(layer['current'])
@@ -133,7 +135,7 @@ for layer in layers:
 
         #Use file title from json file or file name as fallback
         file_title = layer['current']['title'] if 'title' in layer['current'] else layer['current']['file']
-        lbl_filename = tk.Label(master=frame,text=file_title, width=20, anchor='center', font=("Arial", 16))
+        lbl_filename = tk.Label(master=frame,text=file_title, anchor='center', font=("Arial", 16))
 
         btn_left = tk.Button(master=frame,text="<", command=lambda arg1=lbl_filename, arg2=layer:btn_left_handler(arg1, arg2) )
         btn_right = tk.Button(master=frame,text=">", command=lambda arg1=lbl_filename, arg2=layer:btn_right_handler(arg1,arg2))
@@ -141,9 +143,9 @@ for layer in layers:
         lbl_layer_title.grid(row=i, column=0, columnspan=3, sticky="ew")
         i+=1
 
-        btn_left.grid(row=i, column=0)
-        lbl_filename.grid(row=i, column=1)
-        btn_right.grid(row=i, column=2)
+        btn_left.grid(row=i, column=0, sticky='w')
+        lbl_filename.grid(row=i, column=1, sticky='we')
+        btn_right.grid(row=i, column=2, sticky='e')
         i+=1
 
         separator.grid(row=i, column=0, columnspan=3, pady=(10,0), sticky="ew")
@@ -186,6 +188,7 @@ lbl_saved.grid(row=1, column=0)
 
 window.minsize(900,600)
 window.columnconfigure(0, weight=1)
+
 window.rowconfigure(0, weight=1)
 
 window.mainloop()
