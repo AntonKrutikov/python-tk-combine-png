bcfed6dbc49f80f049ae6a29c777184eec9dbece
antonkrutikov <kap@reso-med.com>
2022-01-15 01:04:23 +0300

    Viewer future: can input nft index now to fast travel

--- viewer.py
+++ viewer.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import os
+from turtle import st
 import widget.image_viewer as image_viewer
 import widget.vscroll_frame as vscroll_frame
 import json
@@ -43,8 +44,20 @@ class Viewer(tk.Tk):
         self.prev_button = tk.Button(self.nav_frame, text='Prev', command=lambda: self.prev_item())
         self.prev_button.grid(column=0, row=0, sticky='ew')
 
-        self.nft_counter = tk.Label(self.nav_frame, text="", foreground="#666")
+        self.nft_counter = tk.Frame(self.nav_frame)
         self.nft_counter.grid(column=1, row=0)
+        self.nft_counter.columnconfigure(0, weight=1)
+        self.nft_counter.columnconfigure(2, weight=1)
+        self.nft_counter_current = tk.Entry(self.nft_counter, border=0, highlightthickness=0, width=3, justify='center', disabledforeground='#222222')
+        self.nft_counter_current.grid(column=0, row=0, sticky='nsew')
+        self.nft_counter_current.bind('<FocusIn>', lambda e: self.nft_counter_current.select_range(0,tk.END))
+        self.nft_counter_current.bind('<Button-1>', lambda e: self.nft_counter_current.configure(state='normal'))
+        self.nft_counter_current.bind('<Return>', self.counter_set_current)
+
+        tk.Label(self.nft_counter, text='/', width=1).grid(column=1, row=0)
+
+        self.nft_counter_total = tk.Entry(self.nft_counter, border=0, highlightthickness=0, width=3, state='disabled',  justify='center', disabledforeground='#222222')
+        self.nft_counter_total.grid(column=2, row=0, sticky='nsew')
 
         self.next_button = tk.Button(self.nav_frame, text='Next', command=lambda: self.next_item())
         self.next_button.grid(column=2, row=0, sticky='ew')
@@ -107,4 +120,22 @@ class Viewer(tk.Tk):
             row+=1
 
     def update_counter(self):
-        self.nft_counter.configure(text="%s/%s" % (self.nft_index + 1, len(self.nft_list)))
+        self.nft_counter_current.configure(state='normal')
+        self.nft_counter_current.delete(0, tk.END)
+        self.nft_counter_current.insert(0, self.nft_index + 1)
+        self.nft_counter.focus_set()
+        self.nft_counter_current.configure(state='disabled')
+        
+        self.nft_counter_total.configure(state='normal')
+        self.nft_counter_total.delete(0, tk.END)
+        self.nft_counter_total.insert(0, len(self.nft_list))
+        self.nft_counter_total.configure(state='disabled')
+
+    def counter_set_current(self, e):
+        val = self.nft_counter_current.get()
+        if not val.isnumeric() or int(val) < 1 or int(val) > len(self.nft_list):
+            return
+        else:
+            self.nft_index = int(val) - 1
+            self.show_item()
+
