6c2244104bd1c844a88b1390b2d9caf5e911f5fc
antonkrutikov <kap@reso-med.com>
2022-01-16 14:27:29 +0300

    Embed generator.py from auto nft-generator with adaption to current classes

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
-For SVG support install https://cairosvg.org/
+`traits.json`
 
-`python3 -m pip install cairosvg`
+Describe all Trait types with file paths and conditions. Also describe ordered/filtered collections.
 
-*In this version depricated using SVG as background layer, because need provide height and width of rendering.
+```json
+{
+  // Type (group) of Trait. 
+  // For example it can be "head", "body", "foot"
+  // Contain 1 or more named traits inside
+  "group": {
+      // Name of Trait variant per group
+      "trait" : {
+          "weight": 1, //not used, always 1
+          // File can be:
+          // 1. String, contain single path
+          // 2. [] of String, contain multiple paths
+          // 3. Object with following attributes
+          //    {
+          //        "path": String or [] of String,
+          //        "adapted-to": [] of String
+          //    }
+          // 4. [] of Objects from (3)
+          "file" : "",
+          // Exclude this Trait from result if other Trait names exists (match by "trait" in any group)
+          "exclude" : ["",""]
+      }
+  }
+
+}
+```
--- generator.py
+++ generator.py
@@ -4,6 +4,9 @@ import csv
 from trait.collection import TraitCollection, TraitCollectionState
 from nft import NFT
 from trait.trait import Trait
+from merge import Merge
+from tqdm import tqdm
+
 
 description="""NFT collection generator
 
@@ -19,22 +22,32 @@ parser.add_argument('--csv', help='CSV file with list of NFT combination', defau
 parser.add_argument('--first-column', help='Name of first column in CSV (used as name)', default='name')
 parser.add_argument('--last-column', help='Name of last column in CSV (used as attribute_number)', default='attribute_number')
 parser.add_argument('--traits', help='JSON description of traits', default='traits.json')
-parser.add_argument('--blueprint', help='JSON template for generating output json file', default='blueprint.json')
+# TODO: check, blueprint for this version respected ny NFT class itself
+# parser.add_argument('--blueprint', help='JSON template for generating output json file', default='blueprint.json')
 parser.add_argument('--out', help='Output folder for results', default='out')
 parser.add_argument('--svg-width', help='Default svg width when convert to png, if svg used as background layer', default=1080, type=int)
 parser.add_argument('--svg-height', help='Default svg height when convert to png, if svg used as background layer', default=1080, type=int)
 parser.add_argument('--use-names', help='Use name column from csv as out filename', action='store_true')
 parser.add_argument('--attribute-number', help='Store or not attribute_number column to result json', action='store_true')
 parser.add_argument('item', help='Index in input csv file to provide only this result instead of all output', default=None, type=int, nargs='?')
+parser.add_argument('--collection-id', help='Id of ordered collection from "_collections" attribute of traits. 0 - default not ordered,', default=0, type=int)
 
-args = parser.parse_args()
+def colored(text, r, g, b):
+    return "\033[38;2;{};{};{}m{}\033[0m".format(r, g, b, text)
 
 class Generator():
-    def __init__(self, args:argparse.Namespace) -> None:
+    def __init__(self, args, merge:Merge) -> None:
         self.args = args
 
-        self.traits = TraitCollection()
-        self.traits.load_from_file(args.traits)
+        collection_list = TraitCollection.load_from_file(args.traits)
+        if args.collection_id < 0 or args.collection_id >= len(collection_list):
+            print('%s: No collection in "traits.json" with index "%s"' % (colored('Error', 255, 0, 0), args.collection_id))
+            exit()
+
+        self.collection_list = collection_list
+        self.traits = collection_list[args.collection_id]
+
+        self.merge = merge
        
         
     def load_csv(self) -> List:
@@ -70,59 +83,92 @@ class Generator():
         return collection
 
     def load_nft(self):
+        errors:Dict[NFT,List[str]] = {}
         csv = self.load_csv()
-        for item in csv:
-            broken = False
-            broken_messages:List[str] = []
-            not_founded:List[Tuple[str,str]] = []
+        if self.args.item is not None:
+            csv = [csv[self.args.item - 1]]
+        saved = 0
+        with tqdm(total=len(csv)) as pbar:
+            for item in csv:
+                broken = False
+                broken_messages:List[str] = []
+                not_founded:List[Tuple[str,str]] = []
 
-            values = [trait_name for _, trait_name in item['traits'].items()]
-            nft = NFT(name=item['name'])
-            nft_traits:TraitCollection = TraitCollection()
-            for group_name, trait_name in item['traits'].items():
-                trait = next((trait for trait in self.traits if trait.name == trait_name and trait.group == group_name), None)
-                if trait is not None:
-                    nft_traits.collection.append(trait)
-                if trait is None and trait_name != '':
-                    not_founded.append((group_name, trait_name))
+                values = [trait_name for _, trait_name in item['traits'].items()]
+                nft = NFT(name=item['name'])
+                nft_traits:TraitCollection = TraitCollection()
+                for group_name, trait_name in item['traits'].items():
+                    trait = next((trait for trait in self.traits if trait.name == trait_name and trait.group == group_name), None)
+                    if trait is not None:
+                        nft_traits.collection.append(trait)
+                    if trait is None and trait_name != '':
+                        not_founded.append((group_name, trait_name))
+                        broken = True
+                        continue
+
+                if broken == True:
+                    broken_messages.append('\tTrait(s) not found in collection: [%s]' % ','.join('"%s" -> "%s"' % (group_name, trait_name) for group_name, trait_name in not_founded))
+
+                # Try to find adapted version of each trait file for current trait set. Get first matched or leave unchanged.
+                for trait in nft_traits.collection:
+                    for file_indx, file in enumerate(trait.files):
+                        for adapt in file.adapted_to:
+                            if adapt in values:
+                                trait.current_file = file
+                                trait.current_file_index = file_indx
+                                break
+
+                nft_traits.groups = nft_traits.make_groups()
+                state = TraitCollectionState(nft_traits)
+
+                excludes = state.excludes(matched_only=True)
+                if len(excludes) > 0:
                     broken = True
-                    continue
+                    for trait, exclude in excludes.items():
+                        broken_messages.append('\tTrait "%s" excluded because next trait value(s) exists in collection: [%s]' % (trait.name, ','.join('"%s"' % name for name,_ in exclude)))
 
-            if broken == True:
-                broken_messages.append('\tTrait(s) not found in collection: [%s]' % ','.join('"%s" -> "%s"' % (group_name, trait_name) for group_name, trait_name in not_founded))
+                conditions = state.conditions()
+                broken_conditions:Dict[Trait,List[str,bool]] = {}
+                for trait, condition in conditions.items():
+                    if all(not ok for name,ok in condition):
+                        if trait not in broken_conditions:
+                            broken_conditions[trait] = condition
+                if len(broken_conditions) > 0:
+                    broken = True
+                    for trait, condition in broken_conditions.items():
+                        broken_messages.append('\tTrait %s not match any adapted_to condition: [%s]' % (trait.name, ','.join('"%s"' % name for name,_ in condition)))
 
-            # Try to find adapted version of each trait file for current trait set. Get first matched or leave unchanged.
-            for trait in nft_traits.collection:
-                for file_indx, file in enumerate(trait.files):
-                    for adapt in file.adapted_to:
-                        if adapt in values:
-                            trait.current_file = file
-                            trait.current_file_index = file_indx
-                            break
+                if broken == True:
+                    errors[nft] = broken_messages
+                else:
+                    attributes = []
+                    order = state.traits.order.json_order
+                    for json_group in order:
+                        for group in state.groups:
+                            if group.name == json_group:
+                                trait, _ = state.current(group)
+                                attributes.append({"trait_type": group.name, "value": trait.name})
+                    nft.attributes = attributes
+                    nft.image = self.merge.combine(state)
+                    if self.args.use_names == True:
+                        nft.file_name = nft.name
+                    ok, err = nft.save()
+                    if not ok:
+                        errors[nft] = ['\t%s' % err]
+                    else:
+                        saved += 1
+                pbar.update(1)
 
-            nft_traits.groups = nft_traits.make_groups()
-            state = TraitCollectionState(nft_traits)
+        print("Successfully saved %s/%s item(s)" % (saved, len(csv)))
 
-            excludes = state.excludes(matched_only=True)
-            if len(excludes) > 0:
-                broken = True
-                for trait, exclude in excludes.items():
-                    broken_messages.append('\tTrait "%s" excluded because next trait value(s) exists in collection: [%s]' % (trait.name, ','.join('"%s"' % name for name,_ in exclude)))
+        for nft in errors:
+            print('%s: NFT "%s" is broken and not saved:' % (colored('Warning', 255, 0, 0), nft.name))
+            for message in errors[nft]:
+                print(message)
+            
 
-            conditions = state.conditions()
-            broken_conditions:Dict[Trait,List[str,bool]] = {}
-            for trait, condition in conditions.items():
-                if all(not ok for name,ok in condition):
-                    if trait not in broken_conditions:
-                        broken_conditions[trait] = condition
-            if len(broken_conditions) > 0:
-                broken = True
-                for trait, condition in broken_conditions.items():
-                    broken_messages.append('\tTrait %s not match any adapted_to condition: [%s]' % (trait.name, ','.join('"%s"' % name for name,_ in condition)))
-
-            if broken == True:
-                print('Warning: NFT "%s" is broken:' % nft.name)
-                for message in broken_messages:
-                    print(message)
-gen = Generator(args)
-gen.load_nft()
+if __name__ == "__main__":
+    args = parser.parse_args()
+    merge = Merge(svg_height=args.svg_height, svg_width=args.svg_width)
+    generator = Generator(args, merge)
+    generator.load_nft()
